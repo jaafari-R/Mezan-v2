9bc248a18dc8e0a9aad46e50a0d52a4a93c3b4d2
Ramadan <jaafariramadan@gmail.com>
2024-07-15 08:05:24 +0300

    created auth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
+import { authController } from "../controllers/auth.controller.js"
 
 const router = Router();
 
+router.use("/register", authController.register);
+router.use("/login", authController.login);
+router.use("/logout", authController.logout);
+router.use("/verify-code", authController.verifyCode);
+router.use("/resend-code", authController.resendCode);
 
 export default router;
