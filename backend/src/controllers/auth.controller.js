1b221f48bd3d86db3be76af693b12fb3007da87a
Ramadan <jaafariramadan@gmail.com>
2024-07-15 07:59:32 +0300

    implemented logout and added comments about each auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,8 +2,16 @@ import errorHandler from "../utils/errorHandler.js";
 
 class AuthController {
     register(req, res) {
+        const { idNumber, firstName, lastName, password, confirmPassword, email, phoneNumber city, address } = req.body;
         try {
+            // validate data
             
+            // does user already exist?
+            
+            // create User
+            // create code
+            // send code to phone / email
+            // go to code page
         }
         catch(error) {
             errorHandler.handleError(res, error);
@@ -11,8 +19,35 @@ class AuthController {
     }
     
     login(req, res) {
+        const { idNumber, password } = req.body;
         try {
+            // validate data
             
+            // match id with password
+            
+            // send token
+        }
+        catch(error) {
+            errorHandler.handleError(res, error);
+        }
+    }
+    
+    verifyRegister(req, res) {
+        const { code } = res.body
+        try {
+            // check code
+            // set user as verified
+            // send token
+        }
+        catch(error) {
+            errorHandler.handleError(res, error);
+        }
+    }
+    
+    resendCode(req, res) {
+        const { idNumber } = req.body;
+        try {
+            // send code to phone/email
         }
         catch(error) {
             errorHandler.handleError(res, error);
@@ -20,11 +55,7 @@ class AuthController {
     }
     
     logout(req, res) {
-        try {
-            
-        }
-        catch(error) {
-            errorHandler.handleError(res, error);
-        }
+        res.clearCookie("authToken")
+        .sendStatus(204);
     }
 }
