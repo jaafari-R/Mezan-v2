38d13a9f7ddfe7d0925f892779c7edfc2237bdcc
Ramadan <jaafariramadan@gmail.com>
2024-07-15 07:40:02 +0300

    initialized auth controller

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,30 @@
+import errorHandler from "../utils/errorHandler.js";
+
+class AuthController {
+    register(req, res) {
+        try {
+            
+        }
+        catch(error) {
+            errorHandler.handleError(res, error);
+        }
+    }
+    
+    login(req, res) {
+        try {
+            
+        }
+        catch(error) {
+            errorHandler.handleError(res, error);
+        }
+    }
+    
+    logout(req, res) {
+        try {
+            
+        }
+        catch(error) {
+            errorHandler.handleError(res, error);
+        }
+    }
+}
